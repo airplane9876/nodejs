799cc5550e541782ee6c8a3f8988a66f12d9b888
airplane9876 <tjdnf2eoeld@naver.com>
2021-05-10 17:37:19 +0900

    why

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(config.mongoURI, {
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 왜 안되나요??')
+  res.send('Hello World! 왜 안되나요??sdad')
 })
 
 app.post('/register', (req, res) => {
