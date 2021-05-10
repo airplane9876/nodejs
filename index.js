d8af21769aea12b23af2032364a7d073c1cac3cf
airplane9876 <tjdnf2eoeld@naver.com>
2021-05-10 17:09:38 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const  {User} = require("./models/Users")
+const bodyParser = require('body-parser')
+const config = require('./config/key')
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kang:qw786796@study.0eocn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB connected..'))
   .catch(err => console.log(err))
@@ -11,6 +18,17 @@ mongoose.connect('mongodb+srv://kang:qw786796@study.0eocn.mongodb.net/myFirstDat
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if(err) return res.json({success : false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
